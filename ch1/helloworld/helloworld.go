a2b217e329e683acdf4baebee05d79bd624635ac
David <https://github.com/DRampling>
2021-07-31 12:39:28 +0100

    feat: go handles unicode so print characters

--- ch1/helloworld/helloworld.go
+++ ch1/helloworld/helloworld.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("こんにちは世界")
 }
+
+// go run .\ch1\helloworld\helloworld.go
+// Compiles source code from files whose names end in .go, links it with libraries, then runs the resulting .exe file
