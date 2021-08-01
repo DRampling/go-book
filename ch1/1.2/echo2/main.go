784316508d0de5007172b983e036e73b1405b39d
David <https://github.com/DRampling>
2021-08-01 21:40:30 +0100

    chore: added notes to echo 2

--- ch1/1.2/echo2/main.go
+++ ch1/1.2/echo2/main.go
@@ -1,4 +1,4 @@
-// Echo2 prints command lines arguments
+// Echo2 prints command lines arguments.
 package main
 
 import (
@@ -14,3 +14,11 @@ func main() {
 	}
 	fmt.Println(s)
 }
+
+// Notes:
+// - os.Args[1:] has n omitted so it defaults to 0 or len(s).
+// - Range produces a pair of values each iteration, the index and value of
+// each element at that index.
+// - Don't need an index so we just use a blank identifer (_) to satisfy the
+// range syntax.
+// - Garbage collection for s on each loop is expensive to looks to use Join.
