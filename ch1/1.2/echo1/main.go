a0ff240d925d7d703fb10cf4aba23a5f8486aaee
David <https://github.com/DRampling>
2021-08-01 21:19:50 +0100

    chore: added package and initialisation notes

--- ch1/1.2/echo1/main.go
+++ ch1/1.2/echo1/main.go
@@ -1,3 +1,4 @@
+// It's considered good practice to describe each package before declaration
 package main
 
 import (
@@ -6,6 +7,7 @@ import (
 )
 
 func main() {
+	// Not explicitly initialized - implicitly set to zero value for zero type
 	var s, sep string
 	for i := 1; 1 < len(os.Args); i++ {
 		s += sep + os.Args[i]
