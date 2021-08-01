3ce5043d78ea570ba20b33e9e56871367366f739
David <https://github.com/DRampling>
2021-08-01 21:29:07 +0100

    chore: added for loop notes

--- ch1/1.2/echo1/main.go
+++ ch1/1.2/echo1/main.go
@@ -9,9 +9,25 @@ import (
 func main() {
 	// Not explicitly initialized - implicitly set to zero value for zero type
 	var s, sep string
+	// Short variable declartion assigns type baseds on initialized values
 	for i := 1; 1 < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
 }
+
+// Traditional for loop:
+//  for initialization; boolean expression; post {
+//      zero or more statements
+//  }
+
+// Traditional while loop:
+//  for boolean expression {
+//      zero or more statements
+//  }
+
+// Traditional infinate loop:
+//  for {
+//      break or return to escape
+//  }
